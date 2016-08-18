3fad7831d7cee89bd34c8fec15c699d3a28d8b52
Eric Mascot <emasco2@uic.edu>
2016-08-18 15:03:00 -0500

    remove preprocessing and change from domain

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,3 @@
----
----
 <?php
 // Check for empty fields
 if(empty($_POST['name'])      ||
@@ -16,10 +14,10 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = '{{site.email_username}}';
+$to = 'emasco2@uic.edu';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@uic.edu\n";
+$headers = "From: noreply@github.com\n";
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
